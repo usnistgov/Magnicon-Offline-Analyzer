43b0d0425bd9c85ada840aa4590818abe009df33
ohm - Shamith Payagala <ohm@nist.gov>
2023-06-28 17:15:06 -0400

    ADD: first commit, add all files

---
+++ bvd_mean.py
@@ -0,0 +1,172 @@
+from magnicon_ccc import magnicon_ccc
+# import numpy as np
+from numpy import sqrt, mean, std
+
+class bvd_mean:
+    def __init__(self, text, T1, T2, P1, P2):
+        mag = magnicon_ccc(text)
+        i = 0
+        self.V1 = []
+        self.V2 = []
+        self.A = []
+        self.B = []
+        self.bvdList = []
+        temp = []
+        start = False
+        points = 0
+        cur = ''
+        while (i < len(mag.dataV)):
+            if i >= len(mag.dataV):
+                break
+            # Start at the first cycle ramping down
+            if mag.phase[i] == 4 and not(start):
+                i += mag.ignored + int((mag.SHC-mag.ignored)/2)
+                start = True
+                cur = 'A1'
+            if start and (points != int((mag.SHC-mag.ignored)/2)) and cur == 'A1':
+                temp.append(mag.dataV[i])
+                points += 1
+            elif start and (points == int((mag.SHC-mag.ignored)/2)) and cur == 'A1':
+                A1 = sum(temp)/len(temp)
+                temp = []
+                points = 0
+                cur = 'B'
+                i += mag.ignored
+                continue
+            if i > len(mag.dataV):
+                break
+            if start and (points != (mag.SHC-mag.ignored)) and cur == 'B':
+                temp.append(mag.dataV[i])
+                points += 1
+            elif start and (points == (mag.SHC-mag.ignored)) and cur == 'B':
+                B1 = sum(temp[0:int((mag.SHC-mag.ignored)/2)])/len(temp[0:int((mag.SHC-mag.ignored)/2)])
+                B2 = sum(temp[int((mag.SHC-mag.ignored)/2):(mag.SHC-mag.ignored)])/len(temp[int((mag.SHC-mag.ignored)/2):(mag.SHC-mag.ignored)])
+                temp = []
+                points = 0
+                cur = 'A2'
+                i += mag.ignored
+                continue
+            if i > len(mag.dataV):
+                break
+            if start and (points != int((mag.SHC-mag.ignored)/2)) and (cur == 'A2'):
+                temp.append(mag.dataV[i])
+                points += 1
+            elif start and (points == int((mag.SHC-mag.ignored)/2)) and cur == 'A2':
+                A2 = sum(temp)/len(temp)
+                self.V1.append(B2-A1)
+                self.V2.append(B1-A2)
+                self.A.append(A1)
+                self.A.append(A2)
+                self.B.append(B1)
+                self.B.append(B2)
+                temp = []
+                points = 0
+                cur = 'A1'
+                continue
+            if i > len(mag.dataV):
+                break
+            i += 1
+
+        self.results(mag, T1, T2, P1, P2)
+
+    def results(self, mag, T1, T2, P1, P2):
+        if 1 != 1:
+            mag = magnicon_ccc()
+        
+        self.k = mag.deltaNApN1/mag.NA
+        R1corr = (mag.R1alpha*(T1-mag.R1stdTemp) + mag.R1beta*(T1-mag.R1stdTemp)**2) - (mag.R1pcr*(P1-101325))/1000
+        R2corr = (mag.R2alpha*(T2-mag.R2stdTemp) + mag.R2beta*(T2-mag.R2stdTemp)**2) - (mag.R2pcr*(P2-101325))/1000
+        self.R1PPM = R1corr + mag.R1Pred
+        self.R2PPM = R2corr + mag.R2Pred
+        self.R1 = (self.R1PPM/1000000 + 1) * mag.R1NomVal
+        self.R2 = (self.R2PPM/1000000 + 1) * mag.R2NomVal
+
+        ratioMeanList = []
+        self.R1List = []
+        self.R2List = []
+        ratioMeanC1 = []
+        ratioMeanC2 = []
+        C1R1List = []
+        C1R2List = []
+        C2R1List = []
+        C2R2List = []
+        for i in range(len(self.V1)):
+            self.bvdList.append((self.V1[i]+self.V2[i])/2)
+            ratioMeanList.append(mag.N1/mag.N2 * (1 + (self.k*mag.NA/mag.N1))*(1 + self.bvdList[i]/mag.deltaI2R2))
+            self.R1List.append((self.R1/ratioMeanList[i] - mag.R2NomVal)/mag.R2NomVal * 10**6 - R2corr)
+            self.R2List.append((self.R2*ratioMeanList[i] - mag.R1NomVal)/mag.R1NomVal * 10**6 - R1corr)
+            ratioMeanC1.append(mag.N1/mag.N2 * (1 + (self.k*mag.NA/mag.N1))*(1 + self.V1[i]/mag.deltaI2R2))
+            ratioMeanC2.append(mag.N1/mag.N2 * (1 + (self.k*mag.NA/mag.N1))*(1 + self.V2[i]/mag.deltaI2R2))
+            C1R1List.append((self.R1/ratioMeanC1[i] - mag.R2NomVal)/mag.R2NomVal * 10**6 - R2corr)
+            C1R2List.append((self.R2*ratioMeanC1[i] - mag.R1NomVal)/mag.R1NomVal * 10**6 - R1corr)
+            C2R1List.append((self.R1/ratioMeanC2[i] - mag.R2NomVal)/mag.R2NomVal * 10**6 - R2corr)
+            C2R2List.append((self.R2*ratioMeanC2[i] - mag.R1NomVal)/mag.R1NomVal * 10**6 - R1corr)
+
+        if self.R1List and self.R2List:
+            # ??
+            self.meanR1 = mean(self.R1List)
+            self.meanR2 = mean(self.R2List)
+            self.stdppm = (std(ratioMeanList, ddof=1))/mean(ratioMeanList)
+            # Show Ali
+            self.stdR1ppm = std(self.R1List, ddof=1)
+            self.stdR2ppm = std(self.R2List, ddof=1)
+            # print(self.stdR1ppm, self.stdR2ppm)
+            # print(self.stdppm*10**6)
+
+            self.stdMeanPPM = self.stdppm/sqrt(len(self.R1List))
+            # Possible problem with R2
+            self.C1R1 = mean(C1R1List)
+            self.C1R2 = mean(C1R2List)
+            self.C2R1 = mean(C2R1List)
+            self.C2R2 = mean(C2R2List)
+            self.stdC1R1 = std(C1R1List, ddof=1)
+            self.stdC1R2 = std(C1R2List, ddof=1)
+            self.stdC2R1 = std(C2R1List, ddof=1)
+            self.stdC2R2 = std(C2R2List, ddof=1)
+        else:
+            self.meanR1 = 0
+            self.meanR2 = 0
+            self.stdppm = 0
+            self.stdMeanPPM = 0
+            self.C1R1 = 0
+            self.C1R2 = 0
+            self.C2R1 = 0
+            self.C2R2 = 0
+            self.stdC1R1 = 0
+            self.stdC1R2 = 0
+            self.stdC2R1 = 0
+            self.stdC2R2 = 0
+
+        if len(self.bvdList):
+            self.mean = mean(self.bvdList)
+            self.std = std(self.bvdList, ddof=1)
+            self.stdMean = self.std/sqrt(len(self.bvdList))
+            self.stdMeanR1 = self.stdR1ppm/sqrt(len(self.R1List))
+            self.stdMeanR2 = self.stdR2ppm/sqrt(len(self.R2List))
+        else:
+            self.bvdMean = 0
+            self.std = 0
+            self.stdMean = 0
+            self.stdMeanR1 = 0
+            self.stdMeanR2 = 0
+        
+        self.ratioMean = mag.N1/mag.N2 * (1 + (self.k*mag.NA/mag.N1))*(1 + self.mean/mag.deltaI2R2)
+
+        ratioMeanChk = mag.N1/mag.N2 * (1 + (self.k*mag.NA/mag.N1))*(1 + mag.bvdMean/mag.deltaI2R2)
+        self.R1MeanChk = (self.R1/ratioMeanChk - mag.R2NomVal)/mag.R2NomVal * 10**6 - R2corr
+        self.R2MeanChk = (self.R2*ratioMeanChk - mag.R1NomVal)/mag.R1NomVal * 10**6 - R1corr
+
+        self.R1CorVal = ((mag.R1Pred/1000000 + 1) * mag.R1NomVal)
+        self.R2CorVal = ((mag.R2Pred/1000000 + 1) * mag.R2NomVal)
+
+        print(self.C1R1-self.C2R1, self.C1R2-self.C2R2)
+        
+        
+        
+if __name__ == '__main__':
+    file1 = r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\High Resistance\2023 AJ\Magnicon Gui Files\2016-02-18_CCC\160218_016_1548.txt'
+    file2 = r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\High Resistance\2023 AJ\Magnicon Gui Files\2023-06-01_CCC\230601_001_1134.txt'
+    file3 = r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\High Resistance\2023 AJ\Magnicon Gui Files\2016-02-18_CCC\160218_001_0935.txt'
+    test = bvd_mean(file2, 25, 25, 101325, 101325)
+    # test = bvd_mean(file2, 25, 25, 103008, 103008)
+    # print(test.ratioMean)
---
+++ magnicon_ccc.py
@@ -0,0 +1,191 @@
+from time import mktime
+from datetime import datetime, timedelta
+import sys
+from numpy import std
+sys.path.append(r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\Ali\py\ResDatabase')
+from ResDataBase import ResData
+
+# Class for parsing CCC files
+class magnicon_ccc:
+    def __init__(self, text):
+        if '.txt' in text:
+            self.validFile = True
+            self.rawFile = text
+            self.bvdFile = text.rstrip('.txt') + '_bvd.txt'
+            self.cfgFile = text.rstrip('.txt') + '_cccdrive.cfg'
+            self.load_raw()
+            self.load_bvd()
+            self.load_cfg()
+            self.calculations()
+        else:
+            self.validFile = False
+
+    # Parses the raw dat (first .txt file)
+    def load_raw(self):
+        if not self.validFile:
+            return
+        self.dataV = []
+        self.phase = []
+        self.error = []
+        self.comments = ''
+        collectData = False
+        with open (self.rawFile, "r") as file:
+            for line in file.readlines():
+                if line.startswith('R1 Info'):
+                    self.R1SN = line.split(':')[-1].rstrip(' \n')
+                    self.R1SN = self.R1SN.lstrip(' \t')
+                if line.startswith('R2 Info'):
+                    self.R2SN = line.split(':')[-1].rstrip(' \n')
+                    self.R2SN = self.R2SN.lstrip(' \t')
+                if line.startswith('number of samples per half cycle'):
+                    self.SHC = int(line.split(':')[-1].rstrip(' \n'))
+                if line.startswith('ignored first samples'):
+                    self.ignored = int(line.split(':')[-1].rstrip(' \n'))
+                    self.samplesUsed = self.SHC - self.ignored
+                if line.startswith('com rel. humid (%)'):
+                    if 'x' in line:
+                        self.relHum = line.split('\t')[-1].rstrip(' \n')
+                    else:
+                        self.relHum = float(line.split(':')[-1].rstrip(' \n'))
+                if line.startswith('remarks'):
+                    self.comments = line.split(':')[-1].rstrip(' \n')
+                    self.comments = self.comments.lstrip(' \t')
+                if line.startswith('stop date'):
+                    if 'x' in line:
+                        stopDate = False
+                    else:
+                        stopDate = True
+                        d1 = [int(line.split('-')[0].lstrip('stop date: \t')), int(line.split('-')[1]), int(line.split('-')[2].rstrip(' \n'))]
+                if line.startswith('start date'):
+                    d2 = [int(line.split('-')[0].lstrip('start date: \t')), int(line.split('-')[1]), int(line.split('-')[2].rstrip(' \n'))]
+                if line.startswith('stop time'):
+                    if 'x' in line:
+                        stopDate = False
+                    else:
+                        stopDate = True
+                        t1 = [int(line.split('.')[0].lstrip('stop time: \t')), int(line.split('.')[1]), int(line.split('.')[2].rstrip(' \n'))]
+                if line.startswith('start time'):
+                    t2 = [int(line.split('.')[0].lstrip('start time: \t')), int(line.split('.')[1]), int(line.split('.')[2].rstrip(' \n'))]
+                if collectData:
+                    self.dataV.append(float(line.split('\t')[0]))
+                    self.phase.append(int(line.split('\t')[1]))
+                    self.error.append(int(line.split('\t')[2]))
+                if line.startswith('data(V)'):
+                    collectData = True
+                if line.startswith('time base (Hz)'):
+                    self.timeBase = line.split(':')[-1].rstrip(' \n')
+                    self.timeBase = int(self.timeBase.lstrip(' \t'))
+                if line.startswith('integration time'):
+                    self.intTime = line.split(':')[-1].rstrip(' \n')
+                    self.intTime = int(self.intTime.lstrip(' \t'))
+
+
+            # Averages the datetime started and stopped and creates a timestamp of the average
+            if stopDate:
+                dt1 = datetime(d1[0], d1[1], d1[2], t1[0], t1[1], t1[2])
+                dt2 = datetime(d2[0], d2[1], d2[2], t2[0], t2[1], t2[2])
+                self.avgDT = (dt1-dt2)/2
+                self.DT = datetime(d2[0], d2[1], d2[2], t2[0], t2[1], t2[2]) + timedelta(days = self.avgDT.days, seconds = self.avgDT.seconds, microseconds = self.avgDT.microseconds)
+                self.timeStamp = mktime(self.DT.timetuple())
+            else:
+                self.DT = datetime(d2[0], d2[1], d2[2], t2[0], t2[1], t2[2])
+                self.timeStamp = mktime(self.DT.timetuple())
+
+            # This does not average
+            # self.timeStamp = mktime(datetime(d2[0], d2[1], d2[2], t2[0], t2[1], t2[2]).timetuple())
+                    
+
+    # Parses the bvd.txt file
+    def load_bvd(self):
+        if not self.validFile:
+            return
+        with open (self.bvdFile, "r") as file:
+            start = False
+            self.bvd = []
+            for line in file.readlines():
+                if line.startswith('delta N1/NA'):
+                    self.deltaNApN1 = float(line.split(':')[-1].rstrip(' \n')) * 0.001
+                if line.startswith('delta (I2*R2)'):
+                    self.deltaI2R2 = float(line.split(':')[-1].rstrip(' \n'))
+                if line.startswith('#points'):
+                    start = True
+                if start and line.split()[0].isnumeric():
+                    self.bvd.append(float(line.split()[1]))
+            array = line.split()
+            if array[0].isnumeric():
+                self.bvdMean = float(array[2])
+                self.stddrt = float(array[3])
+            else:
+                self.bvd = 0
+                self.bvdMean = 0
+                self.stddrt = 0
+            self.bvdStd = std(self.bvd, ddof=1)
+
+    # Parses the .cfg file
+    def load_cfg(self):
+        if not self.validFile:
+            return
+        with open (self.cfgFile, "r") as file:
+            for line in file.readlines():
+                if line.startswith('r1 '):
+                    if ('12906' in line) or ('12.906' in line):
+                        self.R1NomVal = 12906.4037296523
+                    else:
+                        self.R1NomVal = float(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('r2 '):
+                    if ('12906' in line) or ('12.906' in line):
+                        self.R2NomVal = 12906.4037296523
+                    else:
+                        self.R2NomVal = float(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('cs_amplitude 3'):
+                    self.I1 = float(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('cs_amplitude 4'):
+                    self.I2 = float(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('c1_sum'):
+                    self.N1 = int(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('c2_sum'):
+                    self.N2 = int(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('aux_sum'):
+                    self.NA = int(line.split('=')[-1].rstrip(' \n'))
+                    if self.NA == 0:
+                        self.NA = 1
+                if line.startswith("co_amplitude 2"):
+                    self.appVolt = float(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('ra_steptime 2'):
+                    self.rStepTime = int(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('ra_stepcount 2'):
+                    self.rStepCount = int(line.split('=')[-1].rstrip(' \n'))
+                if line.startswith('daq_numcycles_stop'):
+                    self.numCycStop = int(line.split('=')[-1].rstrip(' \n'))
+
+
+    def calculations(self):
+        R = ResData(r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\vax_data\resistor data\ARMS\Analysis Files')
+        self.R1Pred = R.predictedValueUnix(self.R1SN, self.timeStamp)
+        self.R2Pred = R.predictedValueUnix(self.R2SN, self.timeStamp)
+        R1index = R.getSNindex(self.R1SN)
+        R2index = R.getSNindex(self.R2SN)
+
+        self.R1alpha = R.alpha[R1index]
+        self.R1beta = R.beta[R1index]
+        self.R1stdTemp = R.stdTemp[R1index]
+        self.R1pcr = R.pcr[R1index]
+
+        self.R2alpha = R.alpha[R2index]
+        self.R2beta = R.beta[R2index]
+        self.R2stdTemp = R.stdTemp[R2index]
+        self.R2pcr = R.pcr[R2index]
+
+        if self.validFile:
+            self.rampTime = self.rStepTime*self.rStepCount*2/1000000
+            self.fullCyc = self.SHC*self.intTime/self.timeBase * 2
+            self.delay = (self.ignored/self.SHC)*(self.SHC*self.intTime/self.timeBase - self.rampTime)
+            self.measTime = (self.SHC*self.intTime/self.timeBase) - self.rampTime - self.delay
+
+# For testing
+if __name__ == '__main__':
+    file1 = r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\High Resistance\2023 AJ\Magnicon Gui Files\2016-02-18_CCC\160218_016_1548.txt'
+    file2 = r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\High Resistance\2023 AJ\Magnicon Gui Files\2023-06-01_CCC\230601_001_1134.txt'
+    file3 = r'\\elwood.nist.gov\68_PML\68internal\Calibrations\MDSS Data\resist\High Resistance\2023 AJ\Magnicon Gui Files\2016-02-18_CCC\160218_001_0935.txt'
+    mc = magnicon_ccc(file2)
+    # print(mc.R2Pred)
