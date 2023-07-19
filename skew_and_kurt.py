dee3f6dc1b0a6ad914c92e358375b3a3efc815d2
AJ Geckle <ajg8@nist.gov>
2023-07-19 18:05:04 -0400

    Skewness and kurtosis

---
+++ skew_and_kurt.py
@@ -0,0 +1,36 @@
+from numpy import sum, sqrt
+
+def skewness(array):
+    r1, r2, l = default(array)
+    return r2 * (sqrt(l * (l-1))/(l-2))
+
+
+
+def kurtosis(array):
+    r1, r2, l = default(array)
+    return ((((r1-3)*(l+1))+6) * (l-1) * 1/((l-2)*(l-3))) + 3
+
+def default(array):
+    l = len(array)
+    s = sum(array)
+    temp1 = []
+    temp2 = []
+    temp3 = []
+    temp4 = []
+    for i in range(len(array)):
+        temp1.append(array[i] - (s/l))
+        temp2.append((temp1[i])**2)
+        temp3.append(temp2[i]*temp2[i])
+        temp4.append(temp1[i]*temp2[i])
+    temp1 = sum(temp3)
+    temp3 = sum(temp2)/l
+    r1 = sum(temp1)/(temp3*temp3*l)
+    r2 = sum(temp4)/(temp3*sqrt(temp3)*l)
+
+    return r1, r2, l
+
+
+if __name__ == '__main__':
+    from bvd_stats import bvd_stat
+    dat = bvd_stat(r'2023-06-01_CCC\230601_001_1134.txt', T1=25, T2=25, P1=101325, P2=101325)
+    print(kurtosis(dat.bvdList))
